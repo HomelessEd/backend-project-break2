5eabf358d0edcbc0e3a527260fce274506081720
Edward Anderson <edward.a93@hotmail.co.uk>
2026-02-20 15:50:57 +0100

    Helpers Built, moving onto productController.js

--- helpers/baseHtml.js
+++ helpers/baseHtml.js
@@ -1,1 +1,19 @@
-//BaseHTML
+const baseHtml = (title, nav, content) => {
+ return `
+  <!DOCTYPE html>
+   <html lang="en">
+   <head>
+    <meta charset="UTF-8">
+    <title>${title}</title>
+    <link rel="stylesheet" href="/styles.css">
+  </head>
+  <body>
+    ${nav} 
+    <main>
+        ${content}
+    </main>
+  </body>
+  </html>`;
+};
+
+module.exports = baseHtml;
--- helpers/getNavBar.js
+++ helpers/getNavBar.js
@@ -1,1 +1,11 @@
-//getNavBar
+const getNavBar = () => {
+    return `
+    <nav>
+        <a href="/products">Store</a> | 
+        <a href="/dashboard">Admin</a>
+    </nav>
+    <hr>
+    `;
+};
+
+module.exports = getNavBar;
