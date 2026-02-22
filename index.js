d9475b21da4b6e01e3a419cfbd52e3c5222ce919
Edward Anderson <edward.a93@hotmail.co.uk>
2026-02-22 21:08:21 +0100

    Fixed dynamic port for Render

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/', productRoutes);
 app.use('/api', apiRoutes);
 
 //Open Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
