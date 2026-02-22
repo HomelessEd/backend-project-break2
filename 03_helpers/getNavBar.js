46f4c1501709696aa92d5edb018b9f98b57091a8
Edward Anderson <edward.a93@hotmail.co.uk>
2026-02-22 20:39:19 +0100

    Cleanup in NavBar

--- 03_helpers/getNavBar.js
+++ 03_helpers/getNavBar.js
@@ -1,10 +1,17 @@
-const getNavBar = () => {
+const { categories } = require('../02_models/Product'); 
+
+const getNavBar = (isAdmin = false) => {
+    const categoryLinks = categories.map(cat => 
+        `<a href="/products?category=${cat}">${cat}</a>`
+    ).join(' | ');
+
     return `
     <nav class="header-top">
-        <a href="/products">Store</a> | 
+        <a href="/products">All</a> | 
+        ${categoryLinks} | 
         <a href="/dashboard">Admin</a>
+        ${isAdmin ? ' | <a href="/dashboard/new">New Product</a>' : ''}
     </nav>
-    <hr>
     `;
 };
 
